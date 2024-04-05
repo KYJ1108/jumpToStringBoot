fd82c2deaee0eb76faec70928815777dc72f11a8
unknown <kimyeji546408@gmail.com>
2024-04-05 15:14:59 +0900

    question_date_fix

---
+++ src/main/java/com/example/abb4/AnswerRepository.java
@@ -0,0 +1,6 @@
+package com.example.abb4;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+}
--- src/test/java/com/example/abb4/Abb4ApplicationTests.java
+++ src/test/java/com/example/abb4/Abb4ApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.abb4;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-class Abb4ApplicationTests {
+class SbbApplicationTests {
+
+	@Autowired
+	private QuestionRepository questionRepository;
 
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+		Question q = qList.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
-
 }
